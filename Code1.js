fd2efcaaffcf8318728886012ebd893880900070
Youssef <youssefbenhamidaaa@gmail.com>
2024-07-04 21:39:22 +0200

    first commit

---
+++ Code1.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add type annotation for the props
+var Greeting = function (_a) {
+    var name = _a.name;
+    return Hello, { name: name } < /div>;
+};
+exports.default = Greeting;
